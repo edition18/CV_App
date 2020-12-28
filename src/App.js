0b05401d0d3b3d9df169d54ce32099953649a051
Edwin Choo <jchoo004@e.ntu.edu.sg>
2020-12-28 23:18:32 +0800

    decided on using submitForm to master manipulate the CV

--- src/App.js
+++ src/App.js
@@ -5,34 +5,35 @@ import React, { useState, Fragment } from "react";
 import uniqid from "uniqid";
 
 const App = () => {
+  const [submitted, setSubmitted] = useState(false);
   const [name, setName] = useState("ed");
   const [email, setEmail] = useState("e@gmail.com");
   const [phone, setPhone] = useState("231431432");
   const [educations, setEducations] = useState([
     {
       school: "test school",
-      dateStarted: "11/11/2002",
-      dateLeft: "11/11/2010",
+      dateStarted: "2011-12-12",
+      dateLeft: "2013-12-12",
       major: "Tech",
     },
     {
       school: "test school 2",
-      dateStarted: "11/11/2011",
-      dateLeft: "11/11/2018",
+      dateStarted: "2014-12-12",
+      dateLeft: "2018-12-12",
       major: "Biz",
     },
   ]);
   const [practicals, setPracticals] = useState([
     {
       employer: "Mikrosoft",
-      dateStarted: "11/11/2002",
-      dateLeft: "11/11/2010",
+      dateStarted: "2008-12-12",
+      dateLeft: "2014-12-12",
       achievements: ["did this", "and that", "then this"],
     },
     {
       employer: "Epple",
-      dateStarted: "11/11/2012",
-      dateLeft: "11/11/2015",
+      dateStarted: "2015-12-12",
+      dateLeft: "2018-12-12",
       achievements: [
         "did the other thing",
         "and that other thing",
@@ -88,7 +89,10 @@ const App = () => {
     }
   };
 
-  const toggleFormActive = () => {
+  const toggleFormActive = (e) => {
+    e.target.innerHTML === "Show Form"
+      ? (e.target.innerHTML = "Hide Form")
+      : (e.target.innerHTML = "Show Form");
     document.getElementById("form").classList.contains("hideElement")
       ? document.getElementById("form").classList.remove("hideElement")
       : document.getElementById("form").classList.add("hideElement");
@@ -100,20 +104,25 @@ const App = () => {
       <Fragment key={uniqid()}>
         <div id={id}>
           <label>School</label>
-          <input type="text" id={"school"} defaultValue={school}></input>
-          <label>Date Started</label>
           <input
             type="text"
+            id={"school"}
+            defaultValue={school}
+            onChange={handleEducationChange}
+          ></input>
+          <label>Date Started</label>
+          <input
+            type="date"
             id={"dateStarted"}
             defaultValue={dateStarted}
           ></input>
           <label> Date Left</label>
-          <input type="text" id={"dateLeft"} defaultValue={dateLeft}></input>
+          <input type="date" id={"dateLeft"} defaultValue={dateLeft}></input>
           <label>Major</label>
           <input type="text" id={"major"} defaultValue={major}></input>
-          {/* <button className="btn btn-warning" onClick={editEducation} id={id}>
+          <button className="btn btn-warning" onClick={editEducation}>
             Edit Education
-          </button> */}
+          </button>
           <button className="btn btn-danger" id={id} onClick={deleteEducation}>
             Delete Education
           </button>
@@ -133,21 +142,21 @@ const App = () => {
         <input type="text" id={"employer"} defaultValue={employer}></input>
         <label>Date Started</label>
         <input
-          type="text"
+          type="date"
           id={"dateStarted"}
           defaultValue={dateStarted}
         ></input>
         <label> Date Left</label>
-        <input type="text" id={"dateLeft"} defaultValue={dateLeft}></input>
+        <input type="date" id={"dateLeft"} defaultValue={dateLeft}></input>
         <br></br>
         <label>Achievements</label>
         {achievements.map((achievement, index) => (
           <Fragment key={uniqid()}>
-            {console.log()}
             <br></br>
             <input
               type="text"
-              className="form-control"
+              id={index}
+              className="form-control achievements"
               defaultValue={achievement}
             ></input>
             <button
@@ -171,8 +180,8 @@ const App = () => {
     const practicalIndex = Number(e.target.parentNode.id);
     const newAchievement = "";
     setPracticals(
-      practicals.map((practical) =>
-        practicals.indexOf(practical) !== practicalIndex
+      practicals.map((practical, index) =>
+        index !== practicalIndex
           ? practical
           : (practical = {
               ...practical,
@@ -185,7 +194,7 @@ const App = () => {
 
   const addPractical = () => {
     const newPractical = new practicalDetails();
-    setPracticals([...educations, newPractical]);
+    setPracticals([...practicals, newPractical]);
     // since it was an array of educations, we need to set it as an array as well
   };
 
@@ -200,18 +209,29 @@ const App = () => {
           : (practical = {
               ...practical,
               achievements: practical.achievements.filter(
-                (achievement) =>
-                  practical.achievements.indexOf(achievement) !==
-                  achievementIndex
+                (achievement, index) => index !== achievementIndex
               ),
             })
       )
     );
   };
 
+  const handleEducationChange = (e) => {
+    const educationsIndex = Number(e.target.parentNode.id);
+    const propName = e.target.id;
+    console.log(educations);
+    setEducations(
+      educations.map((education, index) =>
+        index !== educationsIndex
+          ? education
+          : (education = { ...education, school: e.target.value })
+      )
+    );
+    console.log(educations);
+  };
+
   const editEducation = (e) => {
     let index = Number(e.target.parentNode.id);
-    console.log();
     const childNodes = e.target.parentNode.childNodes;
     const updatedEducation = new educationDetails();
 
@@ -236,11 +256,6 @@ const App = () => {
         // strict equivalence, need to convert e.target.id to integer!
       )
     );
-
-    // alternative
-    // const copy = educations;
-    // copy.splice(e.target.id, 1);
-    // setEducations([...copy]);
   };
 
   const addEducation = () => {
@@ -255,6 +270,37 @@ const App = () => {
 
   const checkPracticals = () => {
     console.log(practicals);
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    //educations
+    //create a educationDetail for each index in educations
+    for (let i = 0; i < educations.length; i++) {
+      let newEducationArray = [];
+      newEducationArray.push(new educationDetails());
+    }
+
+    const educationsChildNodes = document.getElementById("educations")
+      .childNodes;
+    for (let i = 0; i < educationsChildNodes.length; i++) {
+      console.log(educationsChildNodes[i].parentNode);
+      // educationsChildNodes[i].tagName ==! "INPUT" ? "" :
+      // newEducationArray
+    }
+
+    // document.getElementById("educations").childNodes;
+
+    // //
+    // //practicals
+    //create a educationDetail for each index in practicals
+    // if id include " " , means its an achievement, populate to the right practical and achievement
+    for (let i = 0; i < practicals.length; i++) {
+      let newPracticalArray = [];
+      newPracticalArray.push(new practicalDetails());
+    }
+    // document.getElementById("practicals").childNodes;
   };
 
   //defaultValue allows u to still write in the input field if you have no onChange attributes
@@ -303,43 +349,59 @@ const App = () => {
         {/* educations section */}
         <br></br>
         <h1>Education</h1>
-        <div className="educations">
+        <div id="educations">
           {educations
             ? educations.map((education, index) =>
                 educationSubsection(index, education)
               )
             : ""}
         </div>
-        <button className="btn btn-success" onClick={addEducation}>
+        <button className="btn btn-primary" onClick={addEducation}>
           Add New Education
         </button>
         <br></br>
         <button onClick={checkEducations}>Check Education</button>
         <br></br>
         <h1>Practical Experience</h1>
-        <div className="practicals">
+        <div id="practicals">
           {practicals
             ? practicals.map((practical, index) =>
                 practicalSubsection(index, practical)
               )
             : ""}
+          <button className="btn btn-primary" onClick={addPractical}>
+            Add New Practical
+          </button>
           <button onClick={checkPracticals}>Check Practicals</button>
         </div>
 
         <br></br>
         <br></br>
-        <button type="submit">Submit Form</button>
+        <button type="submit" onClick={submitForm}>
+          Submit Form
+        </button>
       </form>
     </Fragment>
   );
 
+  const toggle = (e) => {
+    console.log(e.target.value);
+    e.target.innerHTML = "new";
+  };
+
   return (
     <div>
       {form}
-
-      <General />
-      <Education />
-      <Practical />
+      <button onClick={toggle}>Test</button>
+      {submitted === false ? (
+        ""
+      ) : (
+        <Fragment>
+          <Education educations={educations} />
+          <Practical practicals={practicals} />
+          <General name={name} email={email} phone={phone} />
+        </Fragment>
+      )}
     </div>
   );
 };
